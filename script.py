60ae10ce89f4d2b2f5a4851c183c5342de3eb964
Vadym Sereda // F5 <103873512+VadSsS@users.noreply.github.com>
2023-05-30 09:43:26 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,51 +1,74 @@
-import time
-import pyautogui
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
+import pyautogui
+import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
 
 pwd = 'password'
-# Connect driver & chrome profile
-s = Service(r"/Users/zorian/PycharmProjects/autoTest/chromedriver")
-o = webdriver.ChromeOptions()
-o.add_argument("--user-data-dir=/Users/zorian/Library/Application Support/Google/Chrome/")
-o.add_argument("--profile-directory=Profile 4")
-o.add_argument("--disable-blink-features=AutomationControlled")
-o.add_experimental_option("excludeSwitches", ["enable-automation"])
-o.add_experimental_option("useAutomationExtension", False)
-)
+
+os.environ['PATH'] += r"C:\Users\F5Vadym\projects\automatization\chromedriver_win32"
+
+# o = webdriver.ChromeOptions()
+# o.add_argument("--user-data-dir=/Users/F5Vadym/AppData/Local/Google/Chrome/User Data")
+# o.add_argument("--profile-directory=Profile 19")
+# o.add_argument("--disable-blink-features=AutomationControlled")
+# o.add_experimental_option("excludeSwitches", ["enable-automation"])
+# o.add_experimental_option("useAutomationExtension", False)
+#
+# driver = webdriver.Chrome(options=o)
+
+driver = webdriver.Chrome()
 # Open StarkNet Bridge
-browser = webdriver.Chrome(service=s, options=o)
-# browser.execute_cdp_cmd()
-browser.maximize_window()
+driver.maximize_window()
 
 # Read & accept terms
-# terms = browser.find_element(By.XPATH, '//li[15]/h2')
-# time.sleep(2)
-# browser.execute_script("arguments[0].scrollIntoView();", terms)
-# Accept = browser.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/div/button')
-# Accept.click()
-# time.sleep(2)
 
-browser.get('https://starkgate.starknet.io/')
-mainHandle = browser.window_handles
+driver.get('https://starkgate.starknet.io/')
+mainHandle = driver.window_handles
 time.sleep(2)
-# Connect Wallet
-EthWallet = browser.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/button[1]')
-EthWallet.click()
-time.sleep(2)
-CEth = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div/div[2]/div')
-CEth.click()
-time.sleep(3)
-browser.switch_to.window(browser.window_handles[1])
 
-# Password MM
-browser.find_element(By.XPATH, '//*[@id="password"]').send_keys(pwd, Keys.ENTER)
-time.sleep(1)
-browser.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[3]/div[2]/button[2]').click()
-time.sleep(1)
-browser.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
-time.sleep(10)
-browser.close()
-browser.quit()
+try:
+    terms = driver.find_element(By.XPATH, '//li[15]/h2')
+    time.sleep(2)
+    driver.execute_script("arguments[0].scrollIntoView();", terms)
+    Accept = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/div/button')
+    Accept.click()
+    time.sleep(2)
+except NoSuchElementException:
+    error_occurred = True
+    EthWallet = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/button[1]')
+    EthWallet.click()
+    time.sleep(2)
+    CEth = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div/div[2]/div')
+    CEth.click()
+    time.sleep(3)
+    driver.switch_to.window(driver.window_handles[1])
+    #
+    # # Password MM
+    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('password')
+    pyautogui.press('enter')
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[3]/div[2]/button[2]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
+finally:
+    EthWallet = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/button[1]')
+    EthWallet.click()
+    time.sleep(2)
+    CEth = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div/div[2]/div')
+    CEth.click()
+    time.sleep(3)
+    driver.switch_to.window(driver.window_handles[1])
+    #
+    # # Password MM
+    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('password')
+    pyautogui.press('enter')
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[3]/div[2]/button[2]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
+
